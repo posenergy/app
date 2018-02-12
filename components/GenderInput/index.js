1d44820a7ab6c8f594834e2bc137fbbebf7b661d
Olivia Graham <oliviagraham@college.harvard.edu>
2018-02-11 20:59:30 -0500

    Added Gender Input.

---
+++ components/GenderInput/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
+import styles from './styles';
+
+export default class GenderInput extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      gender: '',
+    }
+  }
+  render () {
+    return (
+      <TextInput style={styles.emailInput}
+        placeholder="Gender"
+        onChangeText ={gender => this.setState({gender})}
+      />
+    );
+  }
+};
---
+++ components/NameInput/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    nameInput: {
+        backgroundColor: '#FFFFFF',
+        height: 50,
+        width: 300,
+        borderRadius: 10,
+        marginTop: 15,
+        alignSelf: 'center',
+        textAlign: 'center'
+    },
+});
