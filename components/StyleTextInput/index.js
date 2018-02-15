fea3ccc26c8d87f68e3d3a93856441c076fd2b23
Jada Lee <jadalee@Jadas-MacBook-Air.local>
2018-02-14 19:18:24 -0500

    added line at end of files, set navigation to maindrawer.

--- components/StyleTextInput/index.js
+++ components/StyleTextInput/index.js
@@ -18,24 +18,6 @@ StyleTextInput.propTypes = {
   cap: PropTypes.string,
   passwordSecure: PropTypes.bool,
   changeFunction: PropTypes.func,
-  nameProp: PropTypes.string,
-  sizeProp: PropTypes.number,
-  colorProp: PropTypes.string,
 }
 
 export default StyleTextInput
-
-// KEEPING AS NOTES FOR FUTURE CODE
-// export default class StyleTextInput extends Component {
-//   constructor(props) {
-//     super(props)
-//   }  
-//   render () {
-//     return (
-//       <TextInput style={styles.styleTextInput} 
-//       placeholder={this.props.pholder}
-//       onChangeText={this.props.changeFunction}
-//       />
-//     );
-//   }
-// };
