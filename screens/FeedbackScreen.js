1477190c53c22bd3436a7667450ae06633b1d30a
manasi-m <cyber.manasi@gmail.com>
2018-03-04 17:36:00 -0500

    fix

--- screens/FeedbackScreen.js
+++ screens/FeedbackScreen.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import RNCalendarEvents from 'react-native-calendar-events'
 
 class FeedbackScreen extends React.Component {
   static navigationOptions = {
