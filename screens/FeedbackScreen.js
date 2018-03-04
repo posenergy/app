5577393763743b471f4d638ad70099f52a977a65
manasi-m <cyber.manasi@gmail.com>
2018-03-04 17:33:17 -0500

    add rncalendar

--- screens/FeedbackScreen.js
+++ screens/FeedbackScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import RNCalendarEvents from 'react-native-calendar-events'
 
 class FeedbackScreen extends React.Component {
   static navigationOptions = {
