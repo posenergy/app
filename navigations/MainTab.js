fbb67c8e835b26180a8b387b67719930f10c4007
Olivia Graham <oliviagraham@college.harvard.edu>
2018-03-19 13:27:22 -0400

    Some minor changes

--- navigations/MainTab.js
+++ navigations/MainTab.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { Image } from 'react-native'
 import {TabNavigator } from 'react-navigation' // 1.0.0-beta.14
 
+<<<<<<< HEAD
 import ActivitiesStack from './ActivitiesStack'
 import CalendarScreen from '../screens/CalendarScreen'
 import ProfileScreen from '../screens/ProfileScreen'
+=======
+import PackagesScreen from '../screens/PackagesScreen'
+import FeedbackScreen from '../screens/FeedbackScreen'
+import CheckinScreen from '../screens/CheckinScreen'
+import MessagesScreen from '../screens/MessagesScreen'
+import MomentScreen from '../screens/MomentScreen'
+import ScheduleScreen from '../screens/ScheduleScreen'
+>>>>>>> Some minor changes
 
 import '../components/TabIcons/packages.png'
 import '../components/TabIcons/feedback.png'
@@ -35,8 +44,13 @@ const MainTabNavigator = TabNavigator({
       ),
     }),
   },
+<<<<<<< HEAD
   Calendar: {
     screen: CalendarScreen,
+=======
+  Schedule: {
+    screen: ScheduleScreen,
+>>>>>>> Some minor changes
     navigationOptions: ({ navigation }) => ({
       title: 'Calendar',
     tabBarIcon: ({ tintColor }) => (
@@ -46,8 +60,13 @@ const MainTabNavigator = TabNavigator({
       ),
     }),
   },
+<<<<<<< HEAD
   Profile: {
     screen: ProfileScreen,
+=======
+  Messages: {
+    screen: MomentScreen,
+>>>>>>> Some minor changes
     navigationOptions: ({ navigation }) => ({
       title: 'Profile',
     tabBarIcon: ({ tintColor }) => (
