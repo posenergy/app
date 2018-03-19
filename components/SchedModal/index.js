fdea78a4af4ac321318bbcb33f36ffa210fc2687
Olivia Graham <oliviagraham@college.harvard.edu>
2018-03-19 17:37:26 -0400

    Fixing merge conflicts

---
+++ components/SchedModal/index.js
@@ -0,0 +1,54 @@
+import React, { Component, PropTypes } from "react";
+import {
+  Modal,
+  View,
+  Text,
+  Image,
+} from "react-native";
+import styles from './styles'
+import Button from '../Button'
+
+export default class SchedModal extends Component {
+  state = {
+    modalVisible: true,
+  };
+
+  openModal() {
+    this.setState({modalVisible: true})
+  }
+
+  closeModal() {
+    this.setState({modalVisible: false})
+  }
+
+
+  render() {
+    return (
+        <View style={styles.Container}>
+          <Modal
+              visible={this.state.modalVisible}
+              transparent={true}
+              blurRadius={1}
+              animationType={'slide'}
+              onRequestClose={() => this.closeModal()}>
+            <View style={styles.modalContainer}>
+              <View style={styles.innerContainer}>
+                <Image
+                  source={require('../../images/logo_image.png')}
+                  style={styles.image} />
+                <Text style={styles.title}>{this.props.title}</Text>
+                <Text style={styles.text}>{this.props.text}</Text>
+                <Text style={styles.bold}>{this.props.bold}</Text>
+                  <Button
+                      onClick={() => this.closeModal()}
+                      type='modal'
+                      textColor='white'
+                      text= {this.props.bname}>
+                  </Button>
+              </View>
+            </View>
+          </Modal>
+        </View>
+    )
+  }
+}
---
+++ components/SchedModal/styles.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  Container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'column',
+  },
+  innerContainer: {
+    alignItems: 'center',
+    width: 300,
+    height: 454,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    flexDirection: 'column',
+  },
+  image: {
+    height: 74,
+    width: 74,
+    marginTop: 19,
+    marginBottom: 7,
+  },
+  title: {
+    fontStyle: 'italic',
+    fontSize: 26,
+    textAlign: 'center',
+    lineHeight: 31,
+  },
+  text: {
+    fontSize: 21,
+    textAlign: 'center',
+    lineHeight: 25,
+    width: 276,
+    marginTop: 25,
+    marginBottom: 35,
+  },
+  bold: {
+    fontSize: 21,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    lineHeight: 25,
+    marginBottom: 40,
+  },
+  contentWrap: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    },
+})
