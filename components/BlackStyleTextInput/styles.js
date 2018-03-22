e8d0cbe69e0f55e5eeae4d7c3e39c08af6891490
Olivia Graham <oliviagraham@college.harvard.edu>
2018-03-22 16:31:01 -0400

    Backbone profile screen

--- components/BlackStyleTextInput/styles.js
+++ components/BlackStyleTextInput/styles.js
@@ -1,24 +1,24 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
+  styleTextInput: {
+    borderRadius: 10,
+    textAlign: 'left',
+    backgroundColor: 'rgba(0,0,0,0.00001)', // 40% opaque
+    fontSize: 22,
+    flex: 3,
+    color: 'black',
+    marginBottom: 11,
+  },
   viewStyle: {
     flexDirection: 'row',
     borderBottomWidth: 2,
-    borderBottomColor: '#F2F2F2',
+    borderBottomColor: 'rgba(79, 79, 79, 0.4)',
     width: 300,
     height: 50,
     justifyContent: 'center',
     marginTop: 18,
     marginBottom: 21,
-  },
-  styleTextInput: {
-    borderRadius: 10,
-    textAlign: 'left',
-    fontSize: 22,
-    lineHeight: 26,
-    flex: 3,
-    color: 'rgba(255, 255, 255, 0.6)',
-    marginBottom: 11,
   },
   image: {
     flex: 1,
