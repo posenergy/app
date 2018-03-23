a2aedef55372a597b8f7a3d834de44d902e673ad
Olivia Graham <oliviagraham@college.harvard.edu>
2018-03-23 00:37:06 -0400

    yike

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View } from 'react-native'
+import {View } from 'react-native'
+
 
 class CalendarScreen extends React.Component {
   static navigationOptions = {
@@ -9,12 +10,11 @@ class CalendarScreen extends React.Component {
 
   render() {
     return(
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     </View>
-	)
+  )
   }
 }
 
 
 export default CalendarScreen
-
