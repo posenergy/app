6c24794825c2dd4db7548a455e862a79988830c7
Olivia Graham <oliviagraham@college.harvard.edu>
2018-03-23 12:22:23 -0400

    Beginning calendar picker

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,20 +1,42 @@
-import React from 'react'
-import {View } from 'react-native'
+import React, { Component } from 'react'
+import {
+  DatePickerIOS,
+  View,
+  StyleSheet,
+  Text,
+} from 'react-native'
 
+export default class CalendarScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { chosenDate: new Date() }
 
-class CalendarScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome',
+    this.setDate = this.setDate.bind(this)
   }
 
+  setDate(newDate) {
+    this.setState({chosenDate: newDate})
+  }
 
   render() {
-    return(
-   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    </View>
-  )
+    var convertdate = {this.state.chosenDate}
+    var datestring = convert.toString()
+
+    return (
+      <View style={styles.container}>
+      <Text>{datestring} </Text>
+        <DatePickerIOS
+          date={this.state.chosenDate}
+          onDateChange={this.setDate}
+        />
+      </View>
+    )
   }
 }
 
-
-export default CalendarScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+})
