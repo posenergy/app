b02ee913aabe1fbe318c42b0bc15bd1fe086ed98
Olivia Graham <oliviagraham@college.harvard.edu>
2018-03-27 13:07:43 -0400

    Picker finalized

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,90 +1,18 @@
-import React, { Component } from 'react'
-import {
-  DatePickerIOS,
-  View,
-  StyleSheet,
-  Text,
-} from 'react-native'
-import Button from './../components/Button'
 
-export default class CalendarScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { chosenDate: new Date() };
-    this.setDate = this.setDate.bind(this);
+import React from 'react'
+import { View } from 'react-native'
+
+class CalendarScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
   }
-
-  setDate(newDate) {
-    this.setState({chosenDate: newDate})
-  }
-
-  onClick() {
-      console.log("HEY LOOK BISH");
-      console.log(this.state.chosenDate);
-   }
-
-  getDateString(date) {
-    let hours = date.getHours()
-    let suffix = 'AM'
-    if (hours > 12) {
-      hours -= 12
-      suffix = 'PM'
-    } else if (hours == 12) {
-      suffix = 'PM'
-    }
-    return date.toLocaleDateString() + ' ' + hours + ':' + date.getMinutes() + ' ' + suffix
-   }
-// The state will initially be passed in as a prop. Currently just generates today's date.
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.textField}> Confirm Time </Text>
-        <Text style={styles.textField}
-              textColor = '#545680'>
-          {this.getDateString(this.state.chosenDate)}
-        </Text>
-      <View style={styles.datePicker}>
-        <DatePickerIOS
-          date={this.state.chosenDate}
-          onDateChange={this.setDate}
-        />
-      </View>
-       <Button type='picker'
-          onClick={() => this.onClick()}
-          text='Next' textColor='white'/>
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  textField: {
-    justifyContent: 'center',
-    textAlign: 'center',
-    alignSelf: 'center',
-    fontWeight: 'bold',
-    fontSize: 24,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    width: '100%',
-    height: '7%',
-  },
-  textContainer: {
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    alignItems: 'center',
-    height: 60,
-    width: 375,
-    marginBottom: 50,
-  },
-  datePicker: {
-    marginTop: '5%',
-    backgroundColor: 'white',
-    marginBottom: 50,
-  },
-})
+export default CalendarScreen
