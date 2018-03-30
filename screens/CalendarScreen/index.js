3a0da4690e0f054c5f2c650616bd532f60fdc71e
manasi-m <cyber.manasi@gmail.com>
2018-03-29 23:53:27 -0400

    events from cal show on screen, bug: keeps adding more on refresh when go back to page, linting almost but wont do it all

--- screens/CalendarScreen/index.js
+++ screens/CalendarScreen/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 import { Agenda } from 'react-native-calendars'
 import RNCalendarEvents from 'react-native-calendar-events'
 import styles from './styles'
@@ -69,71 +69,57 @@ export default class CalendarScreen extends Component {
       <Agenda
         items={this.state.items}
         loadItemsForMonth={this.loadItems.bind(this)}
-        selected={'2018-03-23'}
         renderItem={this.renderItem.bind(this)}
         renderEmptyDate={this.renderEmptyDate.bind(this)}
         rowHasChanged={this.rowHasChanged.bind(this)}
         // markingType={'interactive'}
-        // markedDates={{
-        //  '2017-05-08': [{textColor: '#666'}],
-        //  '2017-05-09': [{textColor: '#666'}],
-        //  '2017-05-14': [{startingDay: true, color: 'blue'}, {endingDay: true, color: 'blue'}],
-        //  '2017-05-21': [{startingDay: true, color: 'blue'}],
-        //  '2017-05-22': [{endingDay: true, color: 'gray'}],
-        //  '2017-05-24': [{startingDay: true, color: 'gray'}],
-        //  '2017-05-25': [{color: 'gray'}],
-        //  '2017-05-26': [{endingDay: true, color: 'gray'}]}}
         // monthFormat={'yyyy'}
         // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
         // renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
       />
     )
   }
+
   loadItems(day) {
     const startDate = new Date('2018-03-10')
-    const endDate = new Date('2018-03-25')
-    let eventsList = []
+    const endDate = new Date('2018-04-25')
+    
     RNCalendarEvents.fetchAllEvents(startDate, endDate)
       .then(allEvents => {
+        setTimeout(() => {
         // console.log(allEvents)
         allEvents.forEach(event => {
-          // setTimeout(() => {
-          // if (!event.allDay) {
-          // for (i = -15; i <85; i++){
-          //   const time = day.timestamp + i * 24 * 60 * 60 * 1000
-            // const strTime = this.timeToString(time)
-            const strTime = event.occurrenceDate.split('T')[0]
-            const evName = event.title 
+          const strTime = event.occurrenceDate.split('T')[0]
+          if (!this.state.items[strTime]) {
+            this.state.items[strTime] = []
+          }
+          const startTime = event.startDate.split('T')[1].split('Z')[0]
+          const endTime = event.endDate.split('T')[1].split('Z')[0]
+          const eventLength = ((new Date(event.endDate).getTime()) - (new Date(event.startDate).getTime())) / (1000 * 60 * 60)
+          const eventHeight = (event.allDay) ? 60 : 100
+          const timeRange = (event.allDay) ? 'All Day' : startTime.split(':')[0] + ':' + startTime.split(':')[1] + ' - ' + endTime.split(':')[0] + ':' + endTime.split(':')[1]
+          this.state.items[strTime].push({
+            name: event.title,
+            start: startTime,
+            end: endTime,
+            length: eventLength,
+            timeRange: timeRange,
+            height: eventHeight,
+          })
+        })
+          for (let i = -15; i < 85; i++) {
+            const time = day.timestamp + i * 24 * 60 * 60 * 1000
+            const strTime = this.timeToString(time)
             if (!this.state.items[strTime]) {
               this.state.items[strTime] = []
-              this.state.items[strTime].push({
-                name: event.title,
-                height: Math.max(50, Math.floor(Math.random() * 150)),
-              })
             }
-            console.log("this is strTime", strTime)
-            // eventsList.push({ [date]: event.title,
-            //                 })
-          // }
-          // console.log(eventsList)
-          // if (!this.state.items[strTime]) {
-          //   this.state.items[strTime] = []
-          //   const numItems = Math.floor(Math.random() * 5)
-          //   for (let j = 0; j < numItems; j++) {
-          //     this.state.items[strTime].push({
-          //       name: event.title,
-          //       height: Math.max(50, Math.floor(Math.random() * 150)),
-          //     })
-          //   }
-          // }
-        // }
-        // }, 1000)
-      })
-      const newItems = {}
-      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key]})
-      this.setState({
-        items: newItems,
-      })
+          }
+          const newItems = {}
+          Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key]})
+          this.setState({
+            items: newItems,
+          })
+        }, 1000)
       })
       .catch (error => {
         // console.log(error)
@@ -147,7 +133,7 @@ export default class CalendarScreen extends Component {
         const time = day.timestamp + i * 24 * 60 * 60 * 1000
         const strTime = this.timeToString(time)
         if (!this.state.items[strTime]) {
-          this.state.items[strTime] = []
+            this.state.items[strTime] = []
           const numItems = Math.floor(Math.random() * 5)
           for (let j = 0; j < numItems; j++) {
             this.state.items[strTime].push({
@@ -158,7 +144,7 @@ export default class CalendarScreen extends Component {
         }
       }
       // console.log(this.state.items)
-      console.log(this.getEvents())
+      // console.log(this.getEvents())
       const newItems = {}
       Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key]})
       this.setState({
@@ -170,7 +156,13 @@ export default class CalendarScreen extends Component {
 
   renderItem(item) {
     return (
-      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+      <TouchableOpacity>
+      <View style={[styles.item, {height: item.height}]}>
+      <Text>{item.name}</Text>
+      <Text>{item.timeRange}</Text>
+      
+      </View>
+      </TouchableOpacity>
     )
   }
 
