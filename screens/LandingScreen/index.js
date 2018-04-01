490831876a4964e1736efd702a50ad07c5d55be3
Olivia Graham <oliviagraham@college.harvard.edu>
2018-03-31 21:08:51 -0400

    Final picker modal

--- screens/LandingScreen/index.js
+++ screens/LandingScreen/index.js
@@ -5,7 +5,6 @@ import styles from './styles'
 
 import Logo from '../../components/Logo'
 import Button from '../../components/Button'
-import PickerModal from '../../components/PickerModal'
 
 
 export default class LandingScreen extends React.Component {
@@ -26,7 +25,6 @@ export default class LandingScreen extends React.Component {
         <Button style = {styles.button} type="login" onClick={() => this.props.navigation.navigate('Login')}
           text="Login" textColor="black"
         />
-        <PickerModal/>
         </ScrollView>
       </ImageBackground>
     )
