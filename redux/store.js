2e5187baf9055c27d49ae0bee3ff7f494e472550
Jada Lee <jadalee@Jadas-MacBook-Air.local>
2018-04-01 14:31:11 -0400

    may or may not be a correctly persisted store

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,9 @@
-
-import { applyMiddleware, createStore } from 'redux'
+import { compose, createStore, applyMiddleware } from 'redux'
+import { AsyncStorage } from 'react-native'
+import { persistStore, persistReducer } from 'redux-persist'
+import thunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
+import storage from 'redux-persist/lib/storage'
 
 import rootReducer from './index'
 
@@ -8,41 +11,22 @@ const log = createLogger({
 	collapsed: true,
 })
 
-export const store = createStore(rootReducer, {}, applyMiddleware(log))
+const persistConfig = {
+ key: 'root',
+ storage: storage,
+}
 
-// store.subscribe(() => {
-// })
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// Use the following line of code to autorehydrate and persist your store
+// NOTE!!!!: uncomment and comment persistor.purge() if you want to clear persisting cache
+// export const store = compose(persistedReducer, {}, applyMiddleware(thunk, log))
+export const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
+persistor.purge()
+
+store.subscribe(() => {
+})
 
 export default store
 
-// import { compose, createStore, applyMiddleware } from 'redux'
-// import { AsyncStorage } from 'react-native'
-// import { persistStore, persistReducer } from 'redux-persist'
-// import thunk from 'redux-thunk'
-// import { createLogger } from 'redux-logger'
-// import storage from 'redux-persist/lib/storage'
-
-// import rootReducer from './index'
-
-// const log = createLogger({
-// 	collapsed: true,
-// })
-
-// const persistConfig = {
-//  key: 'root',
-//  storage: storage,
-// }
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-// // Use the following line of code to autorehydrate and persist your store
-// // NOTE!!!!: uncomment and comment persistor.purge() if you want to clear persisting cache
-// // export const store = compose(persistedReducer, {}, applyMiddleware(thunk, log))
-// export const store = createStore(persistedReducer)
-// export const persistor = persistStore(store)
-// persistor.purge()
-
-// store.subscribe(() => {
-// })
-
-// export default store
