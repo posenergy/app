896c3debdcb1772b75ca7436e33c2f5030dbdead
Jada Lee <jadalee@Jadas-MacBook-Air.local>
2018-04-05 21:16:32 -0400

    fixed MomentScreen and got rid of duplicate

--- screens/MomentScreen/index.js
+++ screens/MomentScreen/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {View, Text } from 'react-native'
+import { View, Text } from 'react-native'
+import YouTube from 'react-native-youtube'
 import Thumbnail from 'react-native-thumbnail-video'
-import SchedModal from './../components/SchedModal'
-import Button from './../components/Button'
+import SchedModal from '../../components/SchedModal'
+import Button from '../../components/Button'
 import FlipToggle from 'react-native-flip-toggle-button'
 import styles from './styles'
 
@@ -13,6 +14,7 @@ class MomentScreen extends React.Component {
   super(props)
 
     this.state = {
+      // video: this.props.navigation.state.params.vid,
       title: this.props.navigation.state.params.title,
       pict: this.props.navigation.state.params.pict,
       text: this.props.navigation.state.params.desc,
@@ -27,13 +29,7 @@ class MomentScreen extends React.Component {
         <SchedModal
             title = {this.state.title}
             brand ={this.state.brand}
-            image = {require('./../images/yoga.png')}
-            text = {this.state.text}
-            varelement = {        
-            <SchedModal
-            title = {this.state.title}
-            brand ={this.state.brand}
-            image = {require('./../images/yoga.png')}
+            image = {require('../../images/yoga.png')}
             text = {this.state.text}
             varelement = {<YouTube
               videoId="cBPP_izKKSs"
@@ -48,24 +44,24 @@ class MomentScreen extends React.Component {
              
               style={{ alignSelf: 'stretch', height: 142, width: 244 }}
             />}
-              varelement2 = {
-                <View style={styles.toggle}>
-                  <FlipToggle
-                    value={this.state.check}
-                    buttonWidth={34}
-                    buttonHeight={21}
-                    buttonRadius={50}
-                    buttonOffColor={'#E5E5E5'}
-                    sliderOffColor={'white'}
-                    buttonOnColor={'#545680'}
-                    sliderOnColor={'#E5E5E5'}
-                    onToggle={() => this.setState({check: !this.state.check})}
-                  />
-                  <Text style={styles.toggleText}>
-                  Include my recovery time in results.
-                  </Text>
-                </View>
-              }
+            varelement2 = {
+              <View style={styles.toggle}>
+                <FlipToggle
+                  value={this.state.check}
+                  buttonWidth={34}
+                  buttonHeight={21}
+                  buttonRadius={50}
+                  buttonOffColor={'#E5E5E5'}
+                  sliderOffColor={'white'}
+                  buttonOnColor={'#545680'}
+                  sliderOnColor={'#E5E5E5'}
+                  onToggle={() => this.setState({check: !this.state.check})}
+                />
+                <Text style={styles.toggleText}>
+                Include my recovery time in results.
+                </Text>
+              </View>
+            }
             button = {<Button type='schedule'
               justifyContent= 'flex-end'
               onClick={() => {
