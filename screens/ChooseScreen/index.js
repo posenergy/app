0bc8c667f0935ccbe3a0608b1eab11dd25266f49
Jada Lee <30352401+jadaleee@users.noreply.github.com>
2018-04-10 16:45:14 -0400

    Fil buttons (#94)
    * Added buttons
    
    * added sweat indx and 70% of the styling
    
    * added in hard-coded icon, trying to style
    
    * styling minus image
    
    * sweat styling, got icon width up
    
    * Adding proj file and choose screen lower case
    
    * updated fetching moments
    
    * starting to do flatlist of icons
    
    * made object for require statements
    
    * everything there, waiting for feedback from Amelia
    
    * got button to change color, border not working
    
    * filtering improved, buttons change color on Click
    
    * trying to fix filtering
    
    * Filtering complete, yet to add in styling
    
    * linted
    
    * styled
    
    * fixed margin for momentTitle, please don't change capitalization of hiit
    
    * capital lol
    
    * fixed hiit

--- screens/ChooseScreen/index.js
+++ screens/ChooseScreen/index.js
@@ -2,29 +2,48 @@ import React from 'react'
 import { Image, TouchableOpacity, View } from 'react-native'
 import { connect } from 'react-redux'
 
+import { mindVisibility } from '../../redux/actions/mindModalActions'
+import { mvmtVisibility } from '../../redux/actions/mvmtModalActions'
 import { del_all_tags } from '../../redux/actions/momentActions'
 import { del_all_mvmt } from '../../redux/actions/mvmtButtonsActions'
 import { del_all_mind } from '../../redux/actions/mindButtonsActions'
 
 import styles from './styles'
 
+const mapStateToProps = state => ({
+  mind: state.toggleMindVisibility.visible,
+  mvmt: state.toggleMvmtVisibility.visible,
+})
+
 const mapDispatchToProps = {
   del_all_tags,
   del_all_mvmt,
   del_all_mind,
+  mindVisibility,
+  mvmtVisibility,
 }
 
 class ChooseScreen extends React.Component {
 
-  render() {
+  reduxProps = () => {
     this.props.del_all_tags()
     this.props.del_all_mvmt()
     this.props.del_all_mind()
+    if (this.props.mind) {
+      this.props.mindVisibility()
+    }
+    if (this.props.mvmt) {
+      this.props.mvmtVisibility()
+    }
+  }
+
+  render() {
     return(
       <View style={styles.viewStyle}>
 
         <TouchableOpacity style = {styles.mvmtTouch}
           onPress={(event) => {
+          this.reduxProps()
           const { navigate } = this.props.navigation
           navigate('Search', {category: 'movement'})
           }}>
@@ -33,6 +52,7 @@ class ChooseScreen extends React.Component {
 
         <TouchableOpacity
           onPress={(event) => {
+          this.reduxProps()
           const { navigate } = this.props.navigation
           navigate('Search', {category: 'mindfulness'})
           }}>
@@ -45,4 +65,4 @@ class ChooseScreen extends React.Component {
   }
 }
 
-export default connect(null, mapDispatchToProps)(ChooseScreen)
+export default connect(mapStateToProps, mapDispatchToProps)(ChooseScreen)
