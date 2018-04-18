7db665901163016093478de3911b96e6767dfd34
oliviaserena <31606951+oliviaserena@users.noreply.github.com>
2018-04-17 20:00:21 -0400

    Fixed - same content (#112)
    * Fixed
    
    * Changed props
    
    * Removed

--- components/BlackStyleTextInput/index.js
+++ components/BlackStyleTextInput/index.js
@@ -3,28 +3,34 @@ import { View, Image, Text } from 'react-native'
 import styles from './styles'
 import PropTypes from 'prop-types'
 
-const StyleTextInput = ({pholder, changeFunction, cap, passwordSecure, nameProp, sizeProp, imagelink }) => (
-  <View
-  style = {styles.viewStyle}>
-  <Image
-    source={imagelink}
-    style = {styles.image}
-  />
-  <Text
-    style={styles.styleTextInput}
-    autoCapitalize="none">
-  {pholder}
-  </Text>
+const BlackStyleTextInput = ({pholder, sub, imagelink }) => (
+  <View marginBottom='2%' width='80%' alignSelf='center' justifyContent='center' >
+    <View
+    style = {styles.viewStyle}>
+    <Image
+      source={imagelink}
+      alignSelf = 'center'
+      justifyContent= 'flex-start'
+      style = {styles.image}
+    />
+    <Text
+      style={styles.styleTextInput}
+      autoCapitalize="none">
+      {pholder}
+    </Text>
+    </View>
+    <Text style={styles.sub}>
+      {sub}
+    </Text>
   </View>
 )
 
-StyleTextInput.propTypes = {
+BlackStyleTextInput.propTypes = {
   pholder: PropTypes.string,
   cap: PropTypes.string,
-  passwordSecure: PropTypes.bool,
+  sub: PropTypes.string,
   changeFunction: PropTypes.func,
   icon: PropTypes.string,
 }
 
-export default StyleTextInput
-
+export default BlackStyleTextInput
